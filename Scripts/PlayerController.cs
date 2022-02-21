ae6c2de437e7e92cd97f77ad40ab873593fb9191
Psyshyt <85297400+Psyshyt@users.noreply.github.com>
2022-02-21 14:58:56 +0300

    Scripts

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private int speed;
+    [SerializeField] private GameObject losePanel;
+
+    private int lineToMove = 1;
+    public float lineDistance = 4;
+    private float maxSpeed = 110;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Time.timeScale = 1;
+        StartCoroutine(SpeedIncrease());
+    }
+
+    private void Update()
+    {
+        if (SwipeController.swipeRight)
+        {
+            if (lineToMove < 2)
+                lineToMove++;
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if (lineToMove == 2)
+            targetPosition += Vector3.right * lineDistance;
+
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controller.Move(moveDir);
+        else
+            controller.Move(diff);
+    }
+
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "obstacle")
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    private IEnumerator SpeedIncrease()
+    {
+        yield return new WaitForSeconds(6);
+        if (speed < maxSpeed)
+        {
+            speed += 2;
+            StartCoroutine(SpeedIncrease());
+        }
+        
+    }
+}
